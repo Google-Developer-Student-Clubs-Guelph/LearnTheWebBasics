1a56b7a80725ec84cc046a05dad5bb637e6334b1
Adrian Di Tosto <aditosto@uoguelph.ca>
2023-10-04 22:20:18 -0400

    completed production

--- index.js
+++ index.js
@@ -2,24 +2,32 @@
 const getJokeBtn = document.getElementById("getJokeBtn");
 const jokeText = document.getElementById("jokeText");
 
-getJokeBtn.addEventListener("click", () => {
+function apiCall() { 
+
     jokeText.textContent = "Fetching a joke...";
 
-    fetch("https://api.api-ninjas.com/v1/jokes?limit=1", {
-        headers: { 'X-Api-Key': 'p27En2Xor53jGo/LeOd8SQ==YDK8drrLAPYK7BHb' }
+    fetch("https://api.api-ninjas.com/v1/jokes?limit=1", { // add event request url
+        headers: { 'X-Api-Key': 'p27En2Xor53jGo/LeOd8SQ==YDK8drrLAPYK7BHb' } // add api key
     })
-        .then((response) => {
+        // Checks the network response 
+        .then(function(response) {
+
+            // If network response was not a success
             if (!response.ok) throw new Error("Network response was not ok");
             return response.json();
         })
-        .then((data) => {
+        .then(function(data) {
 
+            // Update the HTML tag with the joke fetched from the response
             jokeText.textContent = data[0].joke;
 
         })
-        .catch((error) => {
+
+        // Handles any errors that occur during the fetch operation
+        .catch(function(error) {
             console.error("Error fetching joke:", error);
             jokeText.textContent = "An error occurred while fetching the joke.";
         });
-});
+};
 
+getJokeBtn.addEventListener("click", apiCall)
